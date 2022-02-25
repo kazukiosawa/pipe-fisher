283715c6b5f5f7723d7ad0c2fda12bfa0ba31f16
Shigang Li <shigang.li@inf.ethz.ch>
2022-02-25 14:34:28 +0100

    script

--- bert_hf/run_1f1b.sh
+++ bert_hf/run_1f1b.sh
@@ -5,13 +5,14 @@
 #SBATCH --cpus-per-task=12
 #SBATCH --constraint=gpu
 #SBATCH --partition=debug
-#SBATCH --time=00:10:00
+#SBATCH --time=00:05:00
 #SBATCH --account=g34
 #SBATCH --output=results/%j.o
 
 
 module load daint-gpu
-source ${HOME}/myvenv/bin/activate
+conda activate py38_kfac
+#source ${HOME}/myvenv/bin/activate
 
 link_name=latest.o
 if [ -L $link_name ]; then
@@ -30,7 +31,7 @@ srun python main_bert.py \
         --bert_config_path configs/bert_config_bert-base-uncased.json \
         --max_seq_length 128 \
         --micro_batch_size 32 \
-        --num_optimization_steps 5 \
-        --pipeline_method '1f1b' \
+        --num_optimization_steps 8 \
+        --pipeline_method 'gpipe' \
         --seed 2 \
 
