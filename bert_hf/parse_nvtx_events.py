18433fa55af32247fb2d0189b1d8f861b68e7cc2
kazukiosawa <osawa1021@gmail.com>
2022-03-11 19:05:54 +0100

    Mod to calc stats with ms instead of ns

--- bert_hf/parse_nvtx_events.py
+++ bert_hf/parse_nvtx_events.py
@@ -100,7 +100,7 @@ def get_stats(event_texts):
                        'memset': get_memset_time_in_event,
                        'memcpy': get_memcpy_time_in_event,
                        'sync': get_sync_time_in_event}.items():
-            _times = [f(s, e) for s, e in event_start_end]
+            _times = [f(s, e) / 10**6 for s, e in event_start_end]  # ns -> ms
             mean = 0 if len(_times) < 1 else statistics.mean(_times)
             times[key].append(mean)
             stdev = 0 if len(_times) < 2 else statistics.stdev(_times)
@@ -143,7 +143,9 @@ def get_kernel_timeline(main_event_text, sub_event_texts, main_event_indices=Non
                 timeline[txt].append(get_kernel_start_end_in_event(s, e))
         print(txt, len(timeline[txt]))
 
-    with open(args.pickle_path, 'wb') as f:
+    pickle_path = args.pickle_path_timeline
+    print(f'Writing results to "{pickle_path}"')
+    with open(pickle_path, 'wb') as f:
         pickle.dump(timeline, f)
 
 
@@ -160,24 +162,23 @@ def main():
         if args.event_keywords is not None:
             warnings.warn('As event_texts is specified, event_keywords will be ignored.')
 
-    if args.target == 'stats':
-        get_stats(event_texts)
-    else:
-        main_event_indices = [int(s) for s in args.main_event_indices.split(',')]
-        get_kernel_timeline(args.main_event_text, event_texts, main_event_indices)
+    get_stats(event_texts)
+
+    main_event_indices = [int(s) for s in args.main_event_indices.split(',')]
+    get_kernel_timeline(args.main_event_text, event_texts, main_event_indices)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('sqlite_path', type=str)
-    parser.add_argument('--pickle_path', type=str, default='nvtx_events.pickle')
+    parser.add_argument('--pickle_path_stats', type=str, default='stats.pickle')
+    parser.add_argument('--pickle_path_timeline', type=str, default='timeline.pickle')
     parser.add_argument('--ignore_first_event', action='store_true')
     parser.add_argument('--event_texts', type=str)
     parser.add_argument('--event_keywords', type=str)
     parser.add_argument('--wandb_run_path', type=str, default=None)
     parser.add_argument('--main_event_indices', type=str, default=None)
     parser.add_argument('--main_event_text', type=str)
-    parser.add_argument('--target', choices=['stats', 'timeline'], required=True)
     args = parser.parse_args()
     con = sqlite3.connect(args.sqlite_path)
     main()
