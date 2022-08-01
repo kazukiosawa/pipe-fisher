16ee8dbed8e4bc89548519c857bfeee982d0f121
kazukiosawa <osawa1021@gmail.com>
2022-08-01 19:51:58 +0200

    Mod to plot multiple prof

--- scripts/plot_throughput2.sh
+++ scripts/plot_throughput2.sh
@@ -12,11 +12,12 @@ batch_sizes='1,2,4,8,16,32,64'
 #model=opt-350m
 #batch_sizes='1,2,4,8'
 
-data_path=prof/${model}_time_memory.csv
+data_paths=prof/${model}_time_memory_p100.csv,prof/${model}_time_memory_v100.csv,prof/${model}_time_memory_rtx3090.csv
 
 fig_path=figs/${model}_throughput2_gpipe_1f1b.pdf
 python scripts/plot_throughput2.py \
-    --data_path $data_path \
+    --data_paths $data_paths \
+    --titles P100,V100,RTX3090 \
     --fig_path $fig_path \
     --batch_sizes $batch_sizes \
 
@@ -24,7 +25,8 @@ imgcat $fig_path
 
 fig_path=figs/${model}_throughput2_chimera.pdf
 python scripts/plot_throughput2.py \
-    --data_path $data_path \
+    --data_paths $data_paths \
+    --titles P100,V100,RTX3090 \
     --fig_path $fig_path \
     --chimera \
     --batch_sizes $batch_sizes \
